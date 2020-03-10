d212aae2136fe292567c620eed6f0e8612a27163
Crystal Broach <crysland@gmail.com>
2020-03-09 21:07:51 -0500

    created Composition

---
+++ LegoMinifigures/Composition/Heads/ZoeHead.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegoMinifigures.Composition.Heads
+{
+    class ZoeHead
+    {
+        public bool Helmeted { get; set; }
+        public string FacialExpression { get; set; }
+        public LegoColor Color { get; set; }
+        public LegoColor EyeColor { get; set; }
+
+        public void Talk()
+        {
+            Console.WriteLine($"Zoe says 'Tell my wife I love her very much', while {FacialExpression} emphatically.");
+        }
+
+        public void EatPie(string typeOfPie)
+        {
+            if (typeOfPie == "apple")
+            {
+                Console.WriteLine("MMM....pie");
+            }
+            else
+            {
+                Console.WriteLine("This is not the right pie!");
+            }
+        }
+    }
+
+    enum LegoColor
+    {
+        Yellow,
+        Red,
+        Green,
+        White,
+        Grey,
+        Blue,
+        Black,
+        Orange
+    }
+}
